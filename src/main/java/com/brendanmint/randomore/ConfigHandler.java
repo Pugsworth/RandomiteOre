5849841227b29ef36676d2d88a71bd8b503bb9c1
Pugsworth <kwvortax114@hotmail.com>
2017-06-08 17:42:54 -0600

    Improved config parsing to account for mod ids with hyphens (-).

--- src/main/java/com/brendanmint/randomore/ConfigHandler.java
+++ src/main/java/com/brendanmint/randomore/ConfigHandler.java
@@ -1,6 +1,7 @@
 package com.brendanmint.randomore;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
@@ -8,53 +9,56 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ConfigHandler
 {
-	public static Configuration config;
-	
-	public static boolean randomiteStable;//Stable - Random amount of 1 item. Unstable - Random amount of random items.
-	//Overworld Gen
-	public static int randomiteSpawnTries;//How many times per chunk generator attempts to spawn ore
-	public static int randomiteSpawnSize;//How Large clumps are
-	public static int randomiteSpawnMin;//Lowest height
-	public static int randomiteSpawnMax;//Highest height
-	//Unstable Item Spawn Values
-	public static String[] randomiteUnstableWhiteList = new String[]{};
-	public static String[] randomiteUnstableWhiteListDef = new String[]{"minecraft:coal","minecraft:iron_ore","minecraft:diamond","minecraft:gold_ore","minecraft:dye.4","minecraft:redstone","minecraft:ender_pearl"};
-	public static int randomiteDropsUnstableBase; //When Unstable this is the minimum amount of items ore will drop.
-	public static int randomiteDropsUnstableChance; //When Unstable this random number is the amount of items the ore can drop in addition to the base number.
-	//Stable Item Spawns
-	public static String[] randomiteStableWhiteList = new String[]{};
-	public static String[] randomiteStableWhiteListDef = new String[]{"minecraft:coal-3+6","minecraft:iron_ore-3+5","minecraft:diamond-2+5","minecraft:gold_ore-2+4","minecraft:dye.4-5+12","minecraft:redstone-5+12","minecraft:ender_pearl-1+2"};
-	
-	public static boolean randomiteStableNether;//Same as Above
-	//Nether Gen
-	public static int randomiteSpawnTriesNether;//How many times per chunk generator attempts to spawn nether ore
-	public static int randomiteSpawnSizeNether;//How Large clumps are
-	public static int randomiteSpawnMinNether;//Lowest height
-	public static int randomiteSpawnMaxNether;//Highest height
-	//Nether Item Spawn Values
-	public static String[] randomiteUnstableWhiteListNether = new String[]{};
-	public static String[] randomiteUnstableWhiteListDefNether = new String[]{"minecraft:coal","minecraft:iron_ore","minecraft:diamond","minecraft:gold_ore","minecraft:quartz","minecraft:ender_pearl"};
-	public static int randomiteDropsUnstableBaseNether; //When Unstable this is the minimum amount of items ore will drop.
-	public static int randomiteDropsUnstableChanceNether; //When Unstable this random number is the amount of items the ore can drop in addition to the base number.
-	//Stable Item Spawns
-	public static String[] randomiteStableWhiteListNether = new String[]{};
-	public static String[] randomiteStableWhiteListDefNether = new String[]{"minecraft:coal-5+9","minecraft:iron_ore-4+8","minecraft:diamond-3+6","minecraft:gold_ore-2+6","minecraft:quartz-5+10","minecraft:ender_pearl-2+3"};
-	@SubscribeEvent
+    public static Configuration config;
+
+    public static boolean randomiteStable; //Stable - Random amount of 1 item. Unstable - Random amount of random items.
+    //Overworld Gen
+    public static int randomiteSpawnTries; //How many times per chunk generator attempts to spawn ore
+    public static int randomiteSpawnSize; //How Large clumps are
+    public static int randomiteSpawnMin; //Lowest height
+    public static int randomiteSpawnMax; //Highest height
+    //Unstable Item Spawn Values
+    public static String[] randomiteUnstableWhiteList = new String[]{};
+    public static String[] randomiteUnstableWhiteListDef = new String[]{"minecraft:coal","minecraft:iron_ore","minecraft:diamond","minecraft:gold_ore","minecraft:dye.4","minecraft:redstone","minecraft:ender_pearl"};
+    public static int randomiteDropsUnstableBase; //When Unstable this is the minimum amount of items ore will drop.
+    public static int randomiteDropsUnstableChance; //When Unstable this random number is the amount of items the ore can drop in addition to the base number.
+    //Stable Item Spawns
+    public static String[] randomiteStableWhiteList = new String[]{};
+    public static String[] randomiteStableWhiteListDef = new String[]{"minecraft:coal-3+6","minecraft:iron_ore-3+5","minecraft:diamond-2+5","minecraft:gold_ore-2+4","minecraft:dye.4-5+12","minecraft:redstone-5+12","minecraft:ender_pearl-1+2"};
+
+    public static boolean randomiteStableNether; //Same as Above
+    //Nether Gen
+    public static int randomiteSpawnTriesNether; //How many times per chunk generator attempts to spawn nether ore
+    public static int randomiteSpawnSizeNether; //How Large clumps are
+    public static int randomiteSpawnMinNether; //Lowest height
+    public static int randomiteSpawnMaxNether; //Highest height
+    //Nether Item Spawn Values
+    public static String[] randomiteUnstableWhiteListNether = new String[]{};
+    public static String[] randomiteUnstableWhiteListDefNether = new String[]{"minecraft:coal","minecraft:iron_ore","minecraft:diamond","minecraft:gold_ore","minecraft:quartz","minecraft:ender_pearl"};
+    public static int randomiteDropsUnstableBaseNether; //When Unstable this is the minimum amount of items ore will drop.
+    public static int randomiteDropsUnstableChanceNether; //When Unstable this random number is the amount of items the ore can drop in addition to the base number.
+    //Stable Item Spawns
+    public static String[] randomiteStableWhiteListNether = new String[]{};
+    public static String[] randomiteStableWhiteListDefNether = new String[]{"minecraft:coal-5+9","minecraft:iron_ore-4+8","minecraft:diamond-3+6","minecraft:gold_ore-2+6","minecraft:quartz-5+10","minecraft:ender_pearl-2+3"};
+
+    public static Pattern oreConfigPattern = Pattern.compile("^((?:[^:]+):(?:\\w+))(?:\\.(\\d+))?\\-(\\d+)\\+(\\d+)$");
+        
+    @SubscribeEvent
     public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) 
-	{
+    {
         if (event.getModID().equals(Reference.MOD_ID))
             syncConfig();
     }
 	
-	public static void init(File file) 
-	{
+    public static void init(File file) 
+    {
         config = new Configuration(file);
         syncConfig();
     }
 	
-	public static void syncConfig() 
-	{
-        String cat;//Category
+    public static void syncConfig() 
+    {
+        String cat; //Category
         
         cat = "behavioroverworld";
         config.addCustomCategoryComment(cat, "Overworld Randomite Ore Settings");
--- src/main/java/com/brendanmint/randomore/blocks/BlockRandomite.java
+++ src/main/java/com/brendanmint/randomore/blocks/BlockRandomite.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 import com.brendanmint.randomore.ConfigHandler;
 import com.brendanmint.randomore.Reference;
+import com.brendanmint.randomore.Util;
+import java.util.regex.Matcher;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -14,104 +16,131 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
-public class BlockRandomite extends Block{
+public class BlockRandomite extends Block {
 
-	private static String oreDrop = "None";
-	private static String[] oreHold = {"dye","5"};
-	private static int oreMeta;
-	private static int dropMin;
-	private static int dropMax;
-	
-	public BlockRandomite() 
-	{
-		super(Material.ROCK);
-		
-		setHarvestLevel("pickaxe", 2);
-		setHardness(3.0f);
-		setResistance(5f);
-		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
-		
-		setUnlocalizedName(Reference.randOreBlocks.RANDOMITEORE.getUnlocalisedName());
-		setRegistryName(Reference.randOreBlocks.RANDOMITEORE.getRegistryName());
-	}
-	
-	@Override
-	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) 
-	{
-		setDrop();
-		super.breakBlock(worldIn, pos, state);
-	}
-	
-	@Override
-	public void onBlockDestroyedByExplosion(World worldIn, BlockPos pos, Explosion explosionIn) {
-		oreDrop = "minecraft:coal";
-		dropMax = 3;
-		dropMin = 2;
-		super.onBlockDestroyedByExplosion(worldIn, pos, explosionIn);
-	}
-	
-	@Override
-	public Item getItemDropped(IBlockState state, Random rand, int fortune)
-	{
-		if(ConfigHandler.randomiteStable) return Item.getByNameOrId(oreDrop);
-		else 
-		{
-			oreDrop = ConfigHandler.randomiteUnstableWhiteList[RANDOM.nextInt(ConfigHandler.randomiteUnstableWhiteList.length)];
-			oreMeta = 0;
-			if(oreDrop.contains("."))
-			{
-					oreHold = oreDrop.split("\\.");
-					oreMeta = Integer.parseInt(oreHold[1]);
-					return Item.getByNameOrId(oreHold[0]);
-			}
-			else
-			{
-				oreMeta = 0;
-				return Item.getByNameOrId(oreDrop);
-			}
-		}
-	}
-	
-	@Override
-	public int damageDropped(IBlockState state) 
-	{
-		return oreMeta == 0 ? 0 : oreMeta;
-	}
-	
-	@Override
-	public int quantityDroppedWithBonus(int fortune, Random random)
-	{
-		if(ConfigHandler.randomiteStable)
-			{
-				if(dropMax - dropMin > 0) return dropMin + RANDOM.nextInt(dropMax - dropMin);
-				else return 0;
-			}
-		else return ConfigHandler.randomiteDropsUnstableBase + RANDOM.nextInt(ConfigHandler.randomiteDropsUnstableChance+1);
-	} 
-	
-	public static void setDrop()
-	{
-		if(ConfigHandler.randomiteStable)
-		{
-			oreDrop = ConfigHandler.randomiteStableWhiteList[RANDOM.nextInt(ConfigHandler.randomiteStableWhiteList.length)];
-			if(oreDrop.contains("."))
-			{
-				oreHold = oreDrop.split("[.+-]");
-				//System.out.println("oreHold: " + oreHold.length);
-				dropMax = Integer.parseInt(oreHold[3]);
-				dropMin = Integer.parseInt(oreHold[2]);
-				oreMeta = Integer.parseInt(oreHold[1]);
-				oreDrop = oreHold[0];
-			}
-			else
-			{
-				oreHold = oreDrop.split("[.+-]");
-				//System.out.println("oreHold: " + oreHold.length);
-				dropMax = Integer.parseInt(oreHold[2]);
-				dropMin = Integer.parseInt(oreHold[1]);
-				oreMeta = 0;
-				oreDrop = oreHold[0];
-			}
-		}
-	}
+    private static String oreDrop = "None";
+    // private static String oreHold = "coal";
+    private static int oreMeta;
+    private static int dropMin;
+    private static int dropMax;
+
+    public BlockRandomite() 
+    {
+        super(Material.ROCK);
+
+        setHarvestLevel("pickaxe", 2);
+        setHardness(3.0f);
+        setResistance(5f);
+        setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+
+        setUnlocalizedName(Reference.randOreBlocks.RANDOMITEORE.getUnlocalisedName());
+        setRegistryName(Reference.randOreBlocks.RANDOMITEORE.getRegistryName());
+    }
+
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) 
+    {
+        setDrop();
+        super.breakBlock(worldIn, pos, state);
+    }
+
+    @Override
+    public void onBlockDestroyedByExplosion(World worldIn, BlockPos pos, Explosion explosionIn) {
+        oreDrop = "minecraft:coal";
+        dropMax = 3;
+        dropMin = 2;
+        super.onBlockDestroyedByExplosion(worldIn, pos, explosionIn);
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        if(ConfigHandler.randomiteStable) return Item.getByNameOrId(oreDrop);
+        /*
+        else 
+        {
+            oreDrop = ConfigHandler.randomiteUnstableWhiteList[RANDOM.nextInt(ConfigHandler.randomiteUnstableWhiteList.length)];
+            oreMeta = 0;
+            if(oreDrop.contains("."))
+            {
+                oreHold = oreDrop.split("\\.");
+                oreMeta = Integer.parseInt(oreHold[1]);
+                return Item.getByNameOrId(oreHold[0]);
+            }
+            else
+            {
+                oreMeta = 0;
+                return Item.getByNameOrId(oreDrop);
+            }
+        }
+        */
+        
+        return Item.getByNameOrId("minecraft:cobblestone");
+    }
+
+    @Override
+    public int damageDropped(IBlockState state) 
+    {
+        return oreMeta == 0 ? 0 : oreMeta;
+    }
+
+    @Override
+    public int quantityDroppedWithBonus(int fortune, Random random)
+    {
+        if(ConfigHandler.randomiteStable)
+        {
+            if(dropMax - dropMin > 0) return dropMin + RANDOM.nextInt(dropMax - dropMin);
+            else return 0;
+        }
+        else return ConfigHandler.randomiteDropsUnstableBase + RANDOM.nextInt(ConfigHandler.randomiteDropsUnstableChance+1);
+        // else return 0;
+    } 
+
+    public static void setDrop()
+    {
+        if(ConfigHandler.randomiteStable)
+        {
+            oreDrop = ConfigHandler.randomiteStableWhiteList[RANDOM.nextInt(ConfigHandler.randomiteStableWhiteList.length)];
+
+            Matcher match = ConfigHandler.oreConfigPattern.matcher(oreDrop);
+            match.find();
+
+            try {
+                System.out.println(String.format("%s/%s - %s,%s", match.group(1), match.group(2), match.group(3), match.group(4)));
+
+                oreDrop = match.group(1);
+                oreMeta = Util.parseIntDef(match.group(2), 0);
+                dropMin = Util.parseIntDef(match.group(3), 1);
+                dropMax = Util.parseIntDef(match.group(4), 1);
+            }
+            catch(NumberFormatException e) {
+                oreDrop = "minecraft:coal";
+                oreMeta = 0;
+                dropMin = 1;
+                dropMax = 1;
+                e.printStackTrace();
+            }
+
+            /*
+            if(oreDrop.contains("."))
+            {
+                oreHold = oreDrop.split("[.+-]");
+                System.out.println("oreHold: " + oreHold.length);
+                dropMax = Integer.parseInt(oreHold[3]);
+                dropMin = Integer.parseInt(oreHold[2]);
+                oreMeta = Integer.parseInt(oreHold[1]);
+                oreDrop = oreHold[0];
+            }
+            else
+            {
+                oreHold = oreDrop.split("[.+-]");
+                System.out.println("oreHold: " + oreHold.length);
+                dropMax = Integer.parseInt(oreHold[2]);
+                dropMin = Integer.parseInt(oreHold[1]);
+                oreMeta = 0;
+                oreDrop = oreHold[0];
+            }
+            */
+        }
+    }
 }
